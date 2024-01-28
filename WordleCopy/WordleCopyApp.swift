4276e979ea1cca0538dbbdb4faefb1f65bd49591
johnnyo <johnolszewskim@gmail.com>
2024-01-28 10:17:28 -0800

    Initial Commit

---
+++ WordleCopy/WordleCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCopyApp.swift
+//  WordleCopy
+//
+//  Created by Johnny O on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
