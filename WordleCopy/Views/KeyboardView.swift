a004022599fd6bf1cb7e709b922e5ab45d21edec
johnnyo <johnolszewskim@gmail.com>
2024-03-24 20:54:58 -0700

    switched delete and guess

--- WordleCopy/Views/KeyboardView.swift
+++ WordleCopy/Views/KeyboardView.swift
@@ -11,7 +11,7 @@ struct KeyboardView: View {
     
     let keyboard: [[(String, KeyType)]] = [[("Q", .letter), ("W", .letter), ("E", .letter), ("R", .letter), ("T", .letter), ("Y", .letter), ("U", .letter), ("I", .letter), ("O", .letter), ("P", .letter)],
                                            [("A", .letter), ("S", .letter), ("D", .letter), ("F", .letter), ("G", .letter), ("H", .letter), ("J", .letter), ("K", .letter), ("L", .letter)],
-                                           [("delete.left.fill", .delete),("Z", .letter), ("X", .letter), ("C", .letter), ("V", .letter), ("B", .letter), ("N", .letter), ("M", .letter), ("return", .guess)]]
+                                           [("return", .guess),("Z", .letter), ("X", .letter), ("C", .letter), ("V", .letter), ("B", .letter), ("N", .letter), ("M", .letter), ("delete.left.fill", .delete)]]
     
     @Binding var gameController: WorldGameViewController
     
